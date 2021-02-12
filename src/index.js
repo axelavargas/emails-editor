07e0f489a1d5d0ff989ca67a2e275a37d015da15
Alexandra Vargas <alexandra.vargas@studocu.com>
2021-02-12 19:35:18 +0100

    refactor (EmailsEditor) remove class, instead use functional components

--- src/index.js
+++ src/index.js
@@ -2,31 +2,21 @@
 require('./emails-editor-style.css');
 const { renderEditorComponent, renderEmailListBlocks } = require('./js/render');
 
-/**
- * ------------------------------------------------------------------------
- * Class Definition
- * ------------------------------------------------------------------------
- */
-class EmailsEditor {
-    constructor(container) {
-        this.container = container;
-        this.init();
-    }
+function EmailsEditor(container) {
 
-    // Public
-    init() {
+    const init = () => {
         // render email editor
-        const { listEmails, emailFormInput } = renderEditorComponent(this.container);
+        const { listEmails, emailFormInput } = renderEditorComponent(container);
         // Add events to handle enter, commas, lose focus to generate email blocks
-        this._addEmailsEditorEvents(listEmails, emailFormInput);
+        _addEmailsEditorEvents(listEmails, emailFormInput);
     }
 
-    _addEmailsEditorEvents(listEmails, emailFormInput) {
+    const _addEmailsEditorEvents = (listEmails, emailFormInput) => {
         // Execute a function when the user releases a key on the keyboard
 
         emailFormInput.addEventListener("keydown", (event) => {
             if (event.code === 'Backspace') {
-                const refEmails = document.querySelectorAll(`[data-email-id]`);
+                const refEmails = container.querySelectorAll(`[data-email-id]`);
                 const lastEmailAdded = refEmails[refEmails.length - 1];
                 if (lastEmailAdded) {
                     lastEmailAdded.remove();
@@ -64,7 +54,7 @@ class EmailsEditor {
             // Delete an email
             const closeButtonDomReference = targetDomElement?.dataset?.deleteIdentifierEmailId;
             if (closeButtonDomReference) {
-                const emailToDeleteDomElement = document.querySelector(`[data-email-id="${closeButtonDomReference}"]`);
+                const emailToDeleteDomElement = container.querySelector(`[data-email-id="${closeButtonDomReference}"]`);
                 if (emailToDeleteDomElement) {
                     listEmails.removeChild(emailToDeleteDomElement);
                 }
@@ -73,16 +63,16 @@ class EmailsEditor {
             // Edit an email
             const editActionDomReference = targetDomElement?.dataset?.emailId;
             if (editActionDomReference) {
-                const emailToEditDomElement = document.querySelector(`[data-email-id="${editActionDomReference}"]`);
+                const emailToEditDomElement = container.querySelector(`[data-email-id="${editActionDomReference}"]`);
                 if (emailToEditDomElement) {
                     console.log('i am going to edit');
                 }
             }
 
         });
-
-
     }
+
+    return init()
 }
 
 module.exports = EmailsEditor;
